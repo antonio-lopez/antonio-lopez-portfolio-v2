d81f53e2361b3b9ae7d706f85efff41c2614e2de
Antonio Lopez <pezlop202@gmail.com>
2022-06-10 22:07:14 -0700

    main and project starter

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { AiOutlineFileText } from 'react-icons/ai';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <section className='min-h-main -mt-20 mb-24 flex flex-col justify-center'>
+      <h1 className=' text-lg font-bold lg:text-2xl'>Hi, my name is</h1>
+      <h2 className=' text-4xl font-bold lg:text-7xl'>Antonio Lopez</h2>
+      <p className=' mt-6 text-sm text-gray-700 lg:text-xl'>
+        I create interactive UIs one component at a time.
+      </p>
+      <div className='mt-6 flex flex-wrap items-center gap-4 gap-y-2 text-sm text-gray-600'>
+        {/* resume link */}
+        <a
+          href='/'
+          aria-label='Antonio Lopez resume'
+          className='flex space-x-1 hover:text-blue-500'
+        >
+          <AiOutlineFileText className='h-5 w-5' />
+          <span>Resume</span>
+        </a>
+        {/* github link */}
+        <a
+          href='https://github.com/antonio-lopez'
+          aria-label='Antonio Lopez github profile'
+          className='flex space-x-1 hover:text-blue-500'
+        >
+          <FaGithub className='h-5 w-5' />
+          <span>@antonio-lopez</span>
+        </a>
+        {/* linkedIn link */}
+        <a
+          href='https://www.linkedin.com/in/antoniolopez6/'
+          aria-label='Antonio Lopez linkedin profile'
+          className='flex space-x-1 hover:text-blue-500'
+        >
+          <FaLinkedin className='h-5 w-5' />
+          <span>@antoniolopez6</span>
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Main;
--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,91 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section>
+      <article>
+        <h1 className='text-2xl md:text-4xl'>Projects</h1>
+        <p className='mt-2 text-gray-600'>
+          A list of projects showcasing my frontend skills.
+        </p>
+        <div className='mt-4 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
+          {/* card */}
+          <Link href={'/'}>
+            <div className='rounded-md border scale-100 hover:border-blue-300 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100'>
+              <div className='flex h-full flex-col items-start rounded-md p-4'>
+                <h1>Project Title</h1>
+                <p className='mb-4 text-sm text-gray-700'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+                  voluptas, maiores delectus officia !
+                </p>
+                <Image
+                  src='/screenshot-demo.png'
+                  className='mt-3'
+                  width={1440}
+                  height={792}
+                />
+                <p className='mt-2 inline-block font-medium'>
+                  <mark className='inline-block bg-transparent px-1 pb-2 leading-[0] hover:bg-blue-200'>
+                    More Details →
+                  </mark>
+                </p>
+              </div>
+            </div>
+          </Link>
+          {/* card */}
+          <div className='rounded-md border scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100'>
+            <div className='flex h-full flex-col items-start rounded-md p-4'>
+              <h1>Project Title</h1>
+              <p className='mb-auto text-sm text-gray-700'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+                voluptas, maiores delectus officia !
+              </p>
+              <Image src='/screenshot-demo.png' width={1440} height={792} />
+              <p className='mt-2 inline-block font-medium'>More Details →</p>
+            </div>
+          </div>
+          {/* card */}
+          <div className='rounded-md border scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100'>
+            <div className='flex h-full flex-col items-start rounded-md p-4'>
+              <h1>Project Title</h1>
+              <p className='mb-auto text-sm text-gray-700'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+                voluptas, maiores delectus officia !
+              </p>
+              <Image src='/screenshot-demo.png' width={1440} height={792} />
+              <p className='mt-2 inline-block font-medium'>More Details →</p>
+            </div>
+          </div>
+          {/* card */}
+          <div className='rounded-md border scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100'>
+            <div className='flex h-full flex-col items-start rounded-md p-4'>
+              <h1>Project Title</h1>
+              <p className='mb-auto text-sm text-gray-700'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+                voluptas, maiores delectus officia !
+              </p>
+              <Image src='/screenshot-demo.png' width={1440} height={792} />
+              <p className='mt-2 inline-block font-medium'>More Details →</p>
+            </div>
+          </div>
+          {/* card */}
+          <div className='rounded-md border scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100'>
+            <div className='flex h-full flex-col items-start rounded-md p-4'>
+              <h1>Project Title</h1>
+              <p className='mb-auto text-sm text-gray-700'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+                voluptas, maiores delectus officia !
+              </p>
+              <Image src='/screenshot-demo.png' width={1440} height={792} />
+              <p className='mt-2 inline-block font-medium'>More Details →</p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default Projects;
