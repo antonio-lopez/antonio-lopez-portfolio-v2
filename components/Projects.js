9fb4c230e11f400a94e6a70c40aea84a80580e18
Antonio Lopez <pezlop202@gmail.com>
2022-06-12 17:44:35 -0700

    project update with project list

--- components/Projects.js
+++ components/Projects.js
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { projects } from '../constants/projects';
+import { BsBoxArrowUpRight } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
 
 const Projects = () => {
   return (
@@ -8,11 +11,62 @@ const Projects = () => {
       <article>
         <h1 className='text-2xl md:text-4xl'>Projects</h1>
         <p className='mt-2 text-gray-600'>
-          A list of projects showcasing my frontend skills.
+          A list of projects showcasing my frontend and backend skills.
         </p>
-        <div className='mt-4 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
-          {/* card */}
-          <Link href={'/'}>
+        <div className='mt-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 '>
+          {projects.map((project) => {
+            const { title, description, image, stack, repository, demo, id } =
+              project;
+            return (
+              <div
+                key={id}
+                className='scale-100 rounded-md border transition duration-100 hover:scale-[1.02] hover:border-blue-300 active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100'
+              >
+                <div className='flex h-full flex-col items-start rounded-md p-4'>
+                  <div className='flex w-full items-center justify-between'>
+                    <h1>{title}</h1>
+                    <div className='flex space-x-2'>
+                      <a href={demo} className='hover:text-blue-500'>
+                        <BsBoxArrowUpRight />
+                      </a>
+                      <a href={repository} className='hover:text-blue-500'>
+                        <FaGithub />
+                      </a>
+                    </div>
+                  </div>
+                  <p className='mb-4 text-sm text-gray-700'>{description}</p>
+                  <ul className='flex w-full items-center justify-between  pb-5 text-sm text-gray-700'>
+                    {stack.map((tag, i) => {
+                      return (
+                        <li
+                          className='rounded-md bg-gray-200 px-1.5 py-0.5'
+                          key={i}
+                        >
+                          {tag}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <Image
+                    src={image}
+                    className='mt-3'
+                    width={1440}
+                    height={792}
+                  />
+                  <div></div>
+                  <Link href={demo}>
+                    <p className='mt-2 inline-block font-medium'>
+                      <mark className='inline-block bg-transparent px-1 pb-2 leading-[0] hover:cursor-pointer hover:bg-blue-200'>
+                        More Details →
+                      </mark>
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
+
+          {/* <Link href={'/'}>
             <div className='rounded-md border scale-100 hover:border-blue-300 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100'>
               <div className='flex h-full flex-col items-start rounded-md p-4'>
                 <h1>Project Title</h1>
@@ -34,7 +88,6 @@ const Projects = () => {
               </div>
             </div>
           </Link>
-          {/* card */}
           <div className='rounded-md border scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100'>
             <div className='flex h-full flex-col items-start rounded-md p-4'>
               <h1>Project Title</h1>
@@ -46,7 +99,6 @@ const Projects = () => {
               <p className='mt-2 inline-block font-medium'>More Details →</p>
             </div>
           </div>
-          {/* card */}
           <div className='rounded-md border scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100'>
             <div className='flex h-full flex-col items-start rounded-md p-4'>
               <h1>Project Title</h1>
@@ -58,7 +110,6 @@ const Projects = () => {
               <p className='mt-2 inline-block font-medium'>More Details →</p>
             </div>
           </div>
-          {/* card */}
           <div className='rounded-md border scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100'>
             <div className='flex h-full flex-col items-start rounded-md p-4'>
               <h1>Project Title</h1>
@@ -70,7 +121,6 @@ const Projects = () => {
               <p className='mt-2 inline-block font-medium'>More Details →</p>
             </div>
           </div>
-          {/* card */}
           <div className='rounded-md border scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100'>
             <div className='flex h-full flex-col items-start rounded-md p-4'>
               <h1>Project Title</h1>
@@ -81,7 +131,7 @@ const Projects = () => {
               <Image src='/screenshot-demo.png' width={1440} height={792} />
               <p className='mt-2 inline-block font-medium'>More Details →</p>
             </div>
-          </div>
+          </div> */}
         </div>
       </article>
     </section>
---
+++ constants/projects.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: 'Project Title',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugavoluptas, maiores delectus officia',
+    image: '/screenshot-demo.png',
+    stack: ['React', 'Express', 'Node', 'MongoDB', 'Redux'],
+    repository: '/',
+    demo: '/',
+    id: 0,
+  },
+  {
+    title: 'Project Title',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugavoluptas, maiores delectus officia',
+    image: '/screenshot-demo.png',
+    stack: ['React', 'Express', 'Node', 'MongoDB', 'Redux'],
+    repository: '/',
+    demo: '/',
+    id: 1,
+  },
+  {
+    title: 'Project Title',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugavoluptas, maiores delectus officia',
+    image: '/screenshot-demo.png',
+    stack: ['React', 'Express', 'Node', 'MongoDB', 'Redux'],
+    repository: '/',
+    demo: '/',
+    id: 2,
+  },
+  {
+    title: 'Project Title',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugavoluptas, maiores delectus officia',
+    image: '/screenshot-demo.png',
+    stack: ['React', 'Express', 'Node', 'MongoDB', 'Redux'],
+    repository: '/',
+    demo: '/',
+    id: 3,
+  },
+  {
+    title: 'Project Title',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugavoluptas, maiores delectus officia',
+    image: '/screenshot-demo.png',
+    stack: ['React', 'Express', 'Node', 'MongoDB', 'Redux'],
+    repository: '/',
+    demo: '/',
+    id: 4,
+  },
+  {
+    title: 'Project Title',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugavoluptas, maiores delectus officia',
+    image: '/screenshot-demo.png',
+    stack: ['React', 'Express', 'Node', 'MongoDB', 'Redux'],
+    repository: '/',
+    demo: '/',
+    id: 5,
+  },
+];
